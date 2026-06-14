27f745184f5ceddfee9caf2bfe3846ab5c10a12d
snehayadav23377-sketch <snehayadav23377@gmail.com>
2026-06-14 19:32:14 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -5,6 +5,74 @@ class dog(animal):
     def sound(self):
         print("The dog barks")
 
+class Student:
+    def display(self):
+        print("Hello, Student!")
+
+s = Student()
+s.display()
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+s = Student("Sneha", 20)
+s.show()
+
+class Student:
+    school = "ABC School"
+
+    def __init__(self, name):
+        self.name = name
+
+s1 = Student("Rahul")
+s2 = Student("Sneha")
+
+print(s1.school)
+print(s2.school)
+
+
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+d = Dog()
+
+d.sound()
+d.bark()
+
+class Dog:
+    def sound(self):
+        print("Bark")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+for animal in (Dog(), Cat()):
+    animal.sound()
+
+class Student:
+    def __init__(self):
+        self.__marks = 90
+
+    def show(self):
+        print(self.__marks)
+
+s = Student()
+s.show()
+
+
+
 
 
 
