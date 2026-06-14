a75ab26518a2efd52e0bbb57f9e496e855249637
snehayadav23377-sketch <snehayadav23377@gmail.com>
2026-06-14 18:43:20 +0530

    Refactor array and string operations in Python script

--- array and string.py
+++ array and string.py
@@ -24,3 +24,41 @@ print(text[0])  # Output: 'p'
 
 text = "python"
 print(text[1:4])  # Output: 'tho'
+
+arr = list(map(int, input().split()))
+print(arr)
+
+arr = [10, 20, 30, 40, 50]
+
+for i in arr:
+    print(i)
+
+
+arr = list(map(int, input().split()))
+arr.reverse()
+print(arr)
+
+arr = list(map(int, input().split()))
+
+even = 0
+odd = 0
+
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print("Even:", even)
+print("Odd:", odd)
+
+s = input()
+print(len(s))
+
+s = input()
+ch = input()
+
+if ch in s:
+    print("Present")
+else:
+    print("Not Present")
